f88dfce4be07a1cab6d5565acc0a58d1b89bbcc9
Reshma sudeepa <reshmasudeepa1@gmail.com>
2026-09-22 09:24:45 +0530

    Add FAQPage component with FAQSection

---
+++ FAQpage.tsx
@@ -0,0 +1,9 @@
+import FAQSection from '../components/home/FAQSection';
+
+export function FAQPage() {
+  return (
+    <div className="min-h-screen w-full bg-[#F8FAFC] dark:bg-slate-950">
+      <FAQSection />
+    </div>
+  );
+}
